87d91ca7640f05eca2615e1781af84b65c5982db
Paz <pazsevilla2004@gmail.com>
2026-05-18 20:07:03 -0300

    feat: se agrega la funcionalida de resta y sus tests

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,9 +1,12 @@
 import unittest
-from calculadora import sumar 
+from calculadora import sumar, restar
 
 class TestCalculadora(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(sumar(2, 3), 5)
+
+    def test_restar(self):
+        self.assertEqual(restar(5, 2), 3)  # <--- Test para la nueva funcionalidad
 
 if __name__ == '__main__':
     unittest.main()
